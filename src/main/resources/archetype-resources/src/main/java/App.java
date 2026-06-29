5fa7cdf3685d4a57a7b5a4cb739b54134d254ffe
Arsen Burko <arsenburko67@gmail.com>
2026-06-29 07:46:05 +0200

    init

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $epam.arsen.burko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
